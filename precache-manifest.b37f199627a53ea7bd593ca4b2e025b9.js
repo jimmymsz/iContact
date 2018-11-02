384898333eface56e8d7bd2025e1dc96c885076b
Jimmy <jimmymsz@ymail.com>
2018-11-03 01:24:35 +0700

    Updates

---
+++ precache-manifest.b37f199627a53ea7bd593ca4b2e025b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6caa96cc425298bc2907",
+    "url": "/iContact/static/js/runtime~main.6caa96cc.js"
+  },
+  {
+    "revision": "96662c156542118e7c68",
+    "url": "/iContact/static/js/main.96662c15.chunk.js"
+  },
+  {
+    "revision": "043f1d0feca1d94d1393",
+    "url": "/iContact/static/js/1.043f1d0f.chunk.js"
+  },
+  {
+    "revision": "043f1d0feca1d94d1393",
+    "url": "/iContact/static/css/1.48400bd3.chunk.css"
+  },
+  {
+    "revision": "803e067d0fd125b97bd5e87cb937cb04",
+    "url": "/iContact/index.html"
+  }
+];
